33dce1be5fb95d55dda269e9607fe2e50bcf8c3f
Martin <martinconradpelayo@gmail.com>
2017-03-23 14:28:23 -0700

    martin-nicky

--- starter-code/public/scripts/controllers/aboutController.js
+++ starter-code/public/scripts/controllers/aboutController.js
@@ -1,6 +1,6 @@
 'use strict';
 
-(function(module) {
+(function (module) {
   const aboutController = {};
 
   aboutController.index = () => {
@@ -8,8 +8,12 @@
     repos.requestRepos(repoView.index);
   };
 
-  // TODO: What value is in 'module'? What is the purpose of this line of code?
+  //done TODO: What value is in 'module'? What is the purpose of this line of code?
   // Put your response in this comment...
+
+//The value in this case is the window object because that is what was passed into the function when it is called. This funciton is a IIFE and the purpose of module is to purposefully set the aboutController object and any attatched methods to the global scope.
+
+
   module.aboutController = aboutController;
   
 })(window);
